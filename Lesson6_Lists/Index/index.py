ab35d47f9f2956bce0a73bdb8fdf67e675435e40
DenisGultsev <135717609+DenisGultsev@users.noreply.github.com>
2023-10-02 02:46:59 +1000

    Index

---
+++ Lesson6_Lists/Index/index.py
@@ -0,0 +1,14 @@
+﻿def find_indexes(lst, min_val, max_val):
+    indexes = []
+    for i in range(len(lst)):
+        if min_val <= lst[i] <= max_val:
+            indexes.append(i)
+    return indexes
+
+
+# Пример использования
+lst = [1, 5, 3, 8, 2, 7, 4, 6]
+min_val = 3
+max_val = 6
+result = find_indexes(lst, min_val, max_val)
+print(result)  # [2, 3, 6, 7]
