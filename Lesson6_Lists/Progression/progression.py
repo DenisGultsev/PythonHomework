20c0f5248bf9341a43263b0391444f1b09799fef
DenisGultsev <135717609+DenisGultsev@users.noreply.github.com>
2023-10-02 02:33:20 +1000

    Progression

---
+++ Lesson6_Lists/Progression/progression.py
@@ -0,0 +1,15 @@
+﻿# Ввод значений с клавиатуры
+a1 = int(input("Введите первый элемент прогрессии: "))
+d = int(input("Введите разность прогрессии: "))
+n = int(input("Введите количество элементов: "))
+
+# Создание и заполнение массива
+progression = []
+for i in range(n):
+    element = a1 + (i * d)
+    progression.append(element)
+
+# Вывод массива
+print("Массив арифметической прогрессии:")
+for element in progression:
+    print(element)
